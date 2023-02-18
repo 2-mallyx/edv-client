723ef1f824f0d45811744cb67ca426b344201dc2
Sören Lembke <soeren.lembke@pm.me>
2023-02-18 09:16:41 +0100

    Created empty Tasks for collection and transfer

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
-use hyper::{Client, Request, Body};
-use std::time::Duration;
+use hyper::{Body, Client, Request};
+use std::{io::Error, time::Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
+    let data_transfer = tokio::spawn(data_transfer());
 
+    let data_collection = tokio::spawn(data_collection());
+
+
+    tokio::join!(data_transfer, data_collection);
+
+    Ok(())
+}
+
+
+async fn data_collection() {
     loop {
-        let free_memory = get_free_memory();
-        let request = Request::post("http://localhost:8080/memory")
-            .header("Content-Type", "application/json")
-            .body(Body::from("{}"))
-            .unwrap();
-        let response = client.request(request).await?;
-        println!("Status: {}", response.status());
+        println!("Task2");
 
-        tokio::time::sleep(Duration::from_secs(60)).await;
+        tokio::time::sleep(Duration::from_secs(1)).await;
     }
 }
 
-fn get_free_memory() -> u64 {
-    // Code to retrieve the free memory information goes here.
-    0
+async fn data_transfer(){
+        let http_client = Client::new();
+        loop {
+            let request = Request::post("http://technik.prignitz-compi.de")
+                .header("Content-Type", "application/json")
+                .body(Body::from("{}"))
+                .unwrap();
+
+            match http_client.request(request).await {
+                Ok(response) => println!("Success!"),
+                Err(e) => println!("Error: {}", e),
+            }
+
+            tokio::time::sleep(Duration::from_secs(6)).await;
+        }
 }
