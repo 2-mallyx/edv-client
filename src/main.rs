4e82ab39f2c2cdbdd6042cf119f0eb5a1b1466f4
Sören Lembke <soeren.lembke@pm.me>
2023-02-26 10:58:17 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use windows_sys::service::{ServiceMainFunction, ServiceStatusHandle, ServiceTableEntry, SERVICE_STATUS};
+use windows_sys::winapi::{DWORD, SERVICE_ACCEPT_STOP, SERVICE_CONTROL_STOP, SERVICE_RUNNING, SERVICE_START_PENDING, SERVICE_STOPPED, SERVICE_STOP_PENDING};
+
+
+
 mod data_collection;
 use data_collection::data_collection;
 mod data_transfer;
